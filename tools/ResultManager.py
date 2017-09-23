ef823d96c2e41bfc0ed7d1d9b0431bd3b9b16b03
nschaetti <n.schaetti@gmail.com>
2017-09-23 19:46:21 +0200

    Result manager

--- tools/ResultManager.py
+++ tools/ResultManager.py
@@ -23,6 +23,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
 # Manage and save results
@@ -32,18 +33,21 @@ class ResultManager(object):
     """
 
     # Constructor
-    def __init__(self, name, description, params_dict, n_samples, k=10):
+    def __init__(self, name, description, params_dict, n_samples, k=10, verbose=False):
         """
         Constructor
         :param params_dict:
         """
         # Properties
+        self._name = name
+        self._description = description
         self._params_dict = params_dict
         self._n_samples = n_samples
         self._k = k
         self._fold = 0
         self._sample = 0
         self._n_dim = len(params_dict.keys()) + 2
+        self._verbose = verbose
 
         # Param to dimension
         self._param2dim = dict()
@@ -64,6 +68,12 @@ class ResultManager(object):
 
         # Generate result matrix
         self._result_matrix = self._generate_matrix()
+
+        # Log
+        if self._verbose:
+            print(u"Starting experiment {}".format(name))
+            print(u"Result matrix is of dimension {}".format(self._n_dim))
+        # end if
     # end __init__
 
     ###########################################
@@ -78,6 +88,11 @@ class ResultManager(object):
         :param value:
         :return:
         """
+        if self._verbose:
+            print(u"\tChanging param state to {}".format(pos))
+        # end if
+
+        # Params
         for param in pos.keys():
             self._pos[param] = pos[param]
         # end for
@@ -90,6 +105,10 @@ class ResultManager(object):
         :param n_sample:
         :return:
         """
+        if self._verbose:
+            print(u"\t\tChanging sample state to {}".format(n_sample))
+        # end if
+
         self._sample = n_sample
     # end set_sample_state
 
@@ -100,6 +119,10 @@ class ResultManager(object):
         :param k:
         :return:
         """
+        if self._verbose:
+            print(u"\t\t\tChanging fold state to {}".format(k))
+        # end if
+
         self._fold = k
     # end set_fold_state
 
@@ -119,7 +142,7 @@ class ResultManager(object):
             dim = self._param2dim[param]
 
             # Pos of value
-            pos = self._value2pos[self._pos[param]]
+            pos = self._value2pos[param][self._pos[param]]
 
             # Set
             element_pos[dim] = pos
@@ -131,8 +154,13 @@ class ResultManager(object):
         # Fold
         element_pos[-1] = self._fold
 
+        # Verbose
+        if self._verbose:
+            print(u"\t\t\t\tSuccess rate {}".format(success_rate))
+        # end if
+
         # Set
-        self._result_matrix[type(element_pos)] = success_rate
+        self._result_matrix[tuple(element_pos)] = success_rate
     # end add_result
 
     # Save results
@@ -142,7 +170,22 @@ class ResultManager(object):
         :param output_dir:
         :return:
         """
-        pass
+        # Verbose
+        if self._verbose:
+            print(u"Saving results to {}".format(output_dir))
+        # end if
+
+        # Save global data
+        self._save_global()
+
+        # For each param
+        for param in self._params_dict.keys():
+            # If there is more than
+            # one value.
+            if len(self._params_dict[param]) > 1:
+                self._save_param_data(param)
+            # end if
+        # end for
     # end save
 
     # Add object
@@ -158,6 +201,45 @@ class ResultManager(object):
     ###########################################
     # Private
     ###########################################
+
+    # Save global data
+    def _save_global(self, output_dir):
+        """
+        Save global data
+        :return:
+        """
+        pass
+    # end _save_global
+
+    # Save param data
+    def _save_param_data(self, param, output_dir):
+        """
+        Save param data
+        :return:
+        """
+        # Create directory
+        os.mkdir(os.path.join(output_dir, param))
+
+        # Param dimension
+        dim = self._param2dim[param]
+
+        # Possible values
+        values = self._params_dict[param]
+
+        # Number of values
+        n_values = len(values)
+
+        # All samples
+        all_samples = np.array([])
+
+        # Sample per values
+        for value in values:
+            # All range
+            position = [slice(None)] * self._n_dim
+
+
+        # end for
+    # end _save_param_data
 
     # Generate result matrix
     def _generate_matrix(self):
