810426196df7b7f19eb9b41f531fa640193690a1
Nils Schaetti <nils.schaetti@unine.ch>
2017-10-18 17:03:18 +0200

    Function to create tokenizer

---
+++ tools/functions.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+import nsNLP
+import sys
+
+
+# Create tokenizer
+def create_tokenizer(tokenizer_type, converter_type):
+    """
+    Create tokenizer
+    :param tokenizer: Tokenizer
+    :return:
+    """
+    # Check converter
+    if converter_type == "wv" and tokenizer_type != "spacy_wv":
+        sys.stderr.write(u"Only Spacy tokenizer is possible for word vector!\n")
+        exit()
+    # end if
+
+    # Tokenizer
+    if tokenizer_type == "nltk":
+        tokenizer = nsNLP.tokenization.NLTKTokenizer()
+    elif tokenizer_type == "spacy":
+        tokenizer = nsNLP.tokenization.SpacyTokenizer()
+    elif tokenizer_type == "spacy_wv":
+        tokenizer = nsNLP.tokenization.SpacyTokenizer(original=True)
+    else:
+        sys.stderr.write(u"Unknown tokenizer type!\n")
+        exit()
+    # end if
+
+    # Return tokenizer object
+    return tokenizer
+# end create_tokenizer
