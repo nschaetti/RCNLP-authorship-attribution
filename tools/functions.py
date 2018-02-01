6d48964152aa5e60b88739e3e401f2d19449e608
nschaetti <n.schaetti@gmail.com>
2018-02-02 00:41:12 +0100

    Choose spacy model

--- tools/functions.py
+++ tools/functions.py
@@ -8,19 +8,22 @@ import sys
 
 
 # Create tokenizer
-def create_tokenizer(tokenizer_type):
+def create_tokenizer(tokenizer_type, lang="en_core_web_lg"):
     """
     Create tokenizer
     :param tokenizer_type: Tokenizer
     :return:
     """
+    print(lang)
     # Tokenizer
     if tokenizer_type == "nltk":
         tokenizer = nsNLP.tokenization.NLTKTokenizer()
+    elif tokenizer_type == "nltk-twitter":
+        tokenizer = nsNLP.tokenization.NLTKTweetTokenizer()
     elif tokenizer_type == "spacy":
-        tokenizer = nsNLP.tokenization.SpacyTokenizer()
+        tokenizer = nsNLP.tokenization.SpacyTokenizer(lang=lang)
     elif tokenizer_type == "spacy_wv":
-        tokenizer = nsNLP.tokenization.SpacyTokenizer(original=True)
+        tokenizer = nsNLP.tokenization.SpacyTokenizer(lang=lang, original=True)
     else:
         sys.stderr.write(u"Unknown tokenizer type!\n")
         exit()
@@ -29,3 +32,4 @@ def create_tokenizer(tokenizer_type):
     # Return tokenizer object
     return tokenizer
 # end create_tokenizer
+
