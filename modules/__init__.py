699eb08011ed96d57a55f61ede4930aeb05a9b6d
Nils Schaetti <n.schaetti@gmail.com>
2018-03-21 20:46:10 +0100

    CNN Embeddings

--- modules/__init__.py
+++ modules/__init__.py
@@ -7,4 +7,5 @@ from CNN2DDeepFeatureSelector import CNN2DDeepFeatureSelector
 from CNN2DFeatureSelector import CNN2DFeatureSelector
 from CNNCharacterEmbedding import CNNCharacterEmbedding
 from CNNDeepFeatureSelector import CNNDeepFeatureSelector
+from CNNEmbedding import CNNEmbedding
 from CNNFeatureSelector import CNNFeatureSelector
