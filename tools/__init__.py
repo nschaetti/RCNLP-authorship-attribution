0d25c8001a442926378a57e9fd929647fa921399
nschaetti <n.schaetti@gmail.com>
2018-06-21 19:21:19 +0200

    Tools init

--- tools/__init__.py
+++ tools/__init__.py
@@ -4,10 +4,12 @@
 
 # Imports
 from argument_parsing import parser_esn_training
+from ccsaa_selector import train_ccsaa
+from cgfs_selector import train_cgfs
 from dataset import *
 from features import *
 from functions import get_params, manage_w, converter_in
 from models import *
 
 # All
-__all__ = ["parser_esn_training", "create_tokenizer", "create_transformer", "converter_in", "get_params", "manage_w"]
+__all__ = ["parser_esn_training", "train_ccsaa", "train_cgfs", "create_tokenizer", "create_transformer", "converter_in", "get_params", "manage_w"]
