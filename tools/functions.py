f891c34f3329eb712671ff2b63acd7806dab73ef
nschaetti <n.schaetti@gmail.com>
2018-06-21 19:20:51 +0200

    Dataset start

--- tools/functions.py
+++ tools/functions.py
@@ -56,9 +56,10 @@ def get_params(space):
     feedbacks_sparsity = space['feedbacks_sparsity']
     lang = space['lang'][0][0]
     embedding = space['embedding'][0][0]
+    dataset_start = space['dataset_start']
 
     return reservoir_size, w_sparsity, leak_rate, input_scaling, input_sparsity, spectral_radius, feature, aggregation, \
-           state_gram, feedbacks_sparsity, lang, embedding
+           state_gram, feedbacks_sparsity, lang, embedding, dataset_start
 # end get_params
 
 
